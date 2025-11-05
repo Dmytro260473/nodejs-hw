8beafb93e4ba7ba32628a0447660f1948765f980
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-11-05 21:26:34 +0200

    hc

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 import { Note } from '../models/note.js';
+
 export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
 
+    if (!mongoUrl) {
+      throw new Error('❌ MONGO_URL is not defined. Please set it in environment variables.');
+    }
+
     await mongoose.connect(mongoUrl);
-   console.log('✅ MongoDB connection established successfully');
+    console.log('✅ MongoDB connection established successfully');
 
-   await Note.syncIndexes();
-
+    await Note.syncIndexes();
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
     process.exit(1);
   }
 };
-connectMongoDB.js
