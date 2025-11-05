b3703873c496dbee475f40188b34b32d63c10001
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-11-05 19:58:31 +0200

    hv

---
+++ src/models/note.js
@@ -0,0 +1,18 @@
+import { TAGS } from "../constants/tags.js";
+import { model, Schema } from "mongoose";
+
+
+const noteSchema = new Schema({
+    userId: {type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: {type: String, required: true, trim: true},
+    content: {type: String, required: false, trim: true, default: ""},
+    tag: {type: String, required: false, enum:[...TAGS], default: "Todo" }
+
+  },{
+    timestamps: true,
+    versionKey: false,
+  });
+
+  noteSchema.index({title: "text", content: "text"});
+  export const Note = model("Note", noteSchema);
+
