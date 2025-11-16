69fef5b0fb55453a9ff0ae69b4976b07476fcbb9
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-11-16 21:55:18 +0200

    hv

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,19 +1,14 @@
-import createHttpError from 'http-errors';
+import {HttpError} from "http-errors";
 
 export const errorHandler = (err, req, res, next) => {
-  // Если это ошибка типа http-errors
-  if (err instanceof createHttpError) {
-    return res.status(err.status).json({
-      message: err.message,
+  if (err instanceof HttpError){
+    res.status(err.status).json({
+      message: err.message || err.name,
     });
+    return
   }
-
-  // Для всех остальных ошибок
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message || 'Internal server error',
-  });
-};
+  const isProd = process.env.NODE_ENV === "production";
+	res.status(500).json({
+		message: isProd ? "Something went wrong. Please, try again!" : err.message,
+	})
+}
